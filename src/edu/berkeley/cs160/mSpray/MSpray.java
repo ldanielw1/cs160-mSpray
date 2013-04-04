d759e66a09e93241ed21f381c81c589cc5733aa6
Daniel Wu <ldanielcwu@berkeley.edu>
2013-04-04 00:28:59 -0700

    mSpray: empty app, formFill package

---
+++ src/edu/berkeley/cs160/mSpray/MSpray.java
@@ -0,0 +1,22 @@
+package edu.berkeley.cs160.mSpray;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MSpray extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mspray);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.mspray, menu);
+        return true;
+    }
+
+}
