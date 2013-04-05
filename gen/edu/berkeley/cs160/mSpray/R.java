feeff10bd9421d5d842b73b8fe2fddd089f684e6
Daniel Wu <ldanielcwu@berkeley.edu>
2013-04-05 12:43:54 -0700

    mSpray: first 2 screens

--- gen/edu/berkeley/cs160/mSpray/R.java
+++ gen/edu/berkeley/cs160/mSpray/R.java
@@ -24,18 +24,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int button1=0x7f080001;
+        public static final int button2=0x7f080002;
+        public static final int button3=0x7f080003;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_mspray=0x7f030000;
+        public static final int add_sprayer=0x7f030001;
     }
     public static final class menu {
         public static final int mspray=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int addSprayer=0x7f050006;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int back=0x7f050007;
+        public static final int confirm=0x7f050008;
+        public static final int h1=0x7f050002;
+        public static final int mSprayTitle=0x7f050003;
+        public static final int sprayerIDs=0x7f050005;
+        public static final int startSpray=0x7f050004;
     }
     public static final class style {
         /** 
