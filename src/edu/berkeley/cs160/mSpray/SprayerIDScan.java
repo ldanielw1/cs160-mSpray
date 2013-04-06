5c54fddf9db2c1986a5cf42563456a269a668a84
Brent Batas <brentbatas@gmail.com>
2013-04-06 14:30:47 -0700

    Updated add sprayer page. Refactored XML and Code a little bit, and made button for first add sprayer really big.

--- src/edu/berkeley/cs160/mSpray/SprayerIDScan.java
+++ src/edu/berkeley/cs160/mSpray/SprayerIDScan.java
@@ -7,8 +7,11 @@ import android.os.Handler;
 import android.os.Message;
 import android.view.Menu;
 import android.view.View;
+import android.widget.RelativeLayout.LayoutParams;
+import android.view.ViewGroup.MarginLayoutParams;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class SprayerIDScan extends Activity {
@@ -19,61 +22,68 @@ public class SprayerIDScan extends Activity {
 	Button addSprayer;
 	TextView sprayerName;
 	TextView sprayerName2;
+
+	RelativeLayout scanRFIDLayout;
+	RelativeLayout sprayersLayout;
 	Handler handler;
 	Boolean secondAdd;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_sprayer);
-        rfidPic = (ImageView)findViewById(R.id.add_sprater_imageView_RFIDPic);
-        backButton = (Button)findViewById(R.id.add_sprayer_button_back);
-        confirmButton = (Button)findViewById(R.id.add_sprayer_button_confirm);
-        addSprayer = (Button)findViewById(R.id.add_sprayer_button_addSprayer);
-        sprayerName = (TextView)findViewById(R.id.add_sprayer_textView_PersonName);
-        sprayerName2 = (TextView)findViewById(R.id.add_sprayer_textView_PersonName2);
-        
-        sprayerName.setVisibility(View.INVISIBLE);
-        sprayerName2.setVisibility(View.INVISIBLE);
-        secondAdd = false;
-        
-        backButton.setOnClickListener(new View.OnClickListener() {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_sprayer);
+
+		addSprayer = (Button) findViewById(R.id.add_sprayer_button_addSprayer);
+		backButton = (Button) findViewById(R.id.add_sprayer_button_back);
+		confirmButton = (Button) findViewById(R.id.add_sprayer_button_confirm);
+
+		scanRFIDLayout = (RelativeLayout) findViewById(R.id.scanRFIDLayout);
+		sprayersLayout = (RelativeLayout) findViewById(R.id.sprayersListLayout);
+		sprayerName = (TextView) findViewById(R.id.add_sprayer_textView_PersonName);
+		sprayerName2 = (TextView) findViewById(R.id.add_sprayer_textView_PersonName2);
+
+		// Initial visibility
+		sprayerName.setVisibility(View.GONE);
+		sprayerName2.setVisibility(View.GONE);
+
+		secondAdd = false;
+
+		backButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				Intent intent = new Intent(getApplicationContext(), MSpray.class);
+				Intent intent = new Intent(getApplicationContext(),
+						MSpray.class);
 				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 				startActivity(intent);
 			};
 		});
-        
-        confirmButton.setOnClickListener(new View.OnClickListener() {
+
+		confirmButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				
-				
+				// TODO
 			};
 		});
-        
-       addSprayer.setOnClickListener(new View.OnClickListener() {
+
+		addSprayer.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				rfidPic.setVisibility(View.VISIBLE);
-				addSprayer.setVisibility(View.INVISIBLE);
-				sprayerName.setVisibility(View.INVISIBLE);
-				sprayerName2.setVisibility(View.INVISIBLE);
+				setRFIDScannerVisibility(View.VISIBLE);
 				TimeBomb();
 			};
 		});
-       
-       handler = new Handler() {
+
+		handler = new Handler() {
 			@Override
 			public void handleMessage(Message msg) {
 				switch (msg.what) {
 				case Constants.TIME_BOMB_SWITH: {
-					rfidPic.setVisibility(View.INVISIBLE);
-					addSprayer.setVisibility(View.VISIBLE);
-					sprayerName.setVisibility(View.VISIBLE);
-					if (!secondAdd){
-						addSprayer.setText(addSprayer.getText().toString() + " Another");
+
+					setRFIDScannerVisibility(View.GONE);
+
+					if (!secondAdd) {
+						sprayerName.setVisibility(View.VISIBLE);
+						addSprayer.setText(addSprayer.getText().toString()
+								+ " (additional)");
 					}
-					if (secondAdd){
+					if (secondAdd) {
 						sprayerName2.setVisibility(View.VISIBLE);
 					}
 					secondAdd = true;
@@ -81,11 +91,34 @@ public class SprayerIDScan extends Activity {
 				}
 			}
 		};
-    }
-    
-    
-    public void TimeBomb(){
-    	Thread thread = new Thread() {
+	}
+
+	private void setRFIDScannerVisibility(int vis) {
+		if (vis == View.GONE) {
+			scanRFIDLayout.setVisibility(View.GONE);
+			addSprayer.setVisibility(View.VISIBLE);
+			sprayersLayout.setVisibility(View.VISIBLE);
+			sprayersLayout.setVisibility(View.VISIBLE);
+
+			// Resize button
+			MarginLayoutParams marginParams = new MarginLayoutParams(
+					addSprayer.getLayoutParams());
+			marginParams.setMargins(0, 0, 0, 0);
+			addSprayer.setLayoutParams(marginParams);
+
+			addSprayer.setLayoutParams(new LayoutParams(
+					LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+
+		} else if (vis == View.VISIBLE) {
+			scanRFIDLayout.setVisibility(View.VISIBLE);
+			addSprayer.setVisibility(View.GONE);
+			sprayersLayout.setVisibility(View.GONE);
+			sprayersLayout.setVisibility(View.GONE);
+		}
+	}
+
+	public void TimeBomb() {
+		Thread thread = new Thread() {
 			@Override
 			public void run() {
 				try {
@@ -93,7 +126,7 @@ public class SprayerIDScan extends Activity {
 						wait(3000);
 						handler.sendMessage(Message.obtain(handler,
 								Constants.TIME_BOMB_SWITH));
-	
+
 					}
 				} catch (InterruptedException ex) {
 				}
@@ -102,16 +135,13 @@ public class SprayerIDScan extends Activity {
 		};
 
 		thread.start();
-    }
-    
-    
-    
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.mspray, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.mspray, menu);
+		return true;
+	}
 
 }
