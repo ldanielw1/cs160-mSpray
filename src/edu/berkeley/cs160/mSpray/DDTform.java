6e63210a34548e6ca77b5e05acc3c686b08e0d69
Annie Shin <annieshin@Annies-MacBook-Pro.local>
2013-04-06 16:52:09 -0700

    edited ddtform activity

--- src/edu/berkeley/cs160/mSpray/DDTform.java
+++ src/edu/berkeley/cs160/mSpray/DDTform.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class DDTform extends Activity {
 
@@ -19,39 +20,46 @@ public class DDTform extends Activity {
 	RadioButton canRefilledNo;
 	Button backButton;
 	Button confirmButton;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.ddt);  
-        getData();
-    }
-    
-    public void getData(){
-    	Intent i = new Intent(this, ConfirmDDT.class);
-    	boolean refilled=false;
-    	roomsSprayedValue = (EditText) findViewById(R.id.ddt_edittext_roomsSprayedValue);
-    	sheltersSprayedValue = (EditText) findViewById(R.id.ddt_edittext_sheltersSprayedValue);
-    	roomsUnsprayedValue = (EditText) findViewById(R.id.ddt_edittext_roomsUnsprayedValue);
-    	sheltersUnsprayedValue = (EditText) findViewById(R.id.ddt_edittext_sheltersUnsprayedValue);
-    	canRefilledYes = (RadioButton) findViewById(R.id.ddt_radiobutton_canRefilledYes);
-    	canRefilledNo = (RadioButton) findViewById(R.id.ddt_radiobutton_canRefilledNo);
-    	backButton = (Button) findViewById(R.id.ddt_button_backButton);
-    	confirmButton = (Button) findViewById(R.id.ddt_button_confirmButton);
-    	canRefilled = (RadioGroup) findViewById(R.id.ddt_radiogroup);
-    	int roomsSprayed = Integer.valueOf(roomsSprayedValue.getText().toString());
-    	int sheltersSprayed = Integer.valueOf(sheltersSprayedValue.getText().toString());
-    	int roomsUnsprayed = Integer.valueOf(roomsUnsprayedValue.getText().toString());
-    	int sheltersUnsprayed = Integer.valueOf(sheltersUnsprayedValue.getText().toString());
-    	if(canRefilled.getCheckedRadioButtonId() == R.id.ddt_radiobutton_canRefilledYes){
-    		refilled=true;
-    	}
-    	//ADD STUFF FOR BACK BUTTON AND CONFIRM BUTTON
-    	i.putExtra("roomsSprayed", roomsSprayed);
-    	i.putExtra("roomsUnsprayed", roomsUnsprayed);
-    	i.putExtra("sheltersSprayed", sheltersSprayed);
-    	i.putExtra("sheltersUnsprayed", sheltersUnsprayed);
-    	i.putExtra("canRefilled", refilled);
-    	startActivity(i);	
-    }
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ddt);  
+		getData();
+	}
+
+	public void getData(){
+		Intent i = new Intent(this, ConfirmDDT.class);
+		boolean refilled=false;
+		roomsSprayedValue = (EditText) findViewById(R.id.ddt_edittext_roomsSprayedValue);
+		sheltersSprayedValue = (EditText) findViewById(R.id.ddt_edittext_sheltersSprayedValue);
+		roomsUnsprayedValue = (EditText) findViewById(R.id.ddt_edittext_roomsUnsprayedValue);
+		sheltersUnsprayedValue = (EditText) findViewById(R.id.ddt_edittext_sheltersUnsprayedValue);
+		canRefilledYes = (RadioButton) findViewById(R.id.ddt_radiobutton_canRefilledYes);
+		canRefilledNo = (RadioButton) findViewById(R.id.ddt_radiobutton_canRefilledNo);
+		backButton = (Button) findViewById(R.id.ddt_button_backButton);
+		confirmButton = (Button) findViewById(R.id.ddt_button_confirmButton);
+		canRefilled = (RadioGroup) findViewById(R.id.ddt_radiogroup);
+		if(roomsSprayedValue.getText().toString().equals("")
+				|| sheltersSprayedValue.getText().toString().equals("")
+				|| roomsUnsprayedValue.getText().toString().equals("")
+				|| sheltersUnsprayedValue.getText().toString().equals("")){
+			Toast.makeText(getApplicationContext(), "Please input a value for every text field", Toast.LENGTH_SHORT).show();
+		} else{
+			int roomsSprayed = Integer.valueOf(roomsSprayedValue.getText().toString());
+			int sheltersSprayed = Integer.valueOf(sheltersSprayedValue.getText().toString());
+			int roomsUnsprayed = Integer.valueOf(roomsUnsprayedValue.getText().toString());
+			int sheltersUnsprayed = Integer.valueOf(sheltersUnsprayedValue.getText().toString());
+			if(canRefilled.getCheckedRadioButtonId() == R.id.ddt_radiobutton_canRefilledYes){
+				refilled=true;
+			}
+			//ADD STUFF FOR BACK BUTTON AND CONFIRM BUTTON
+			i.putExtra("roomsSprayed", roomsSprayed);
+			i.putExtra("roomsUnsprayed", roomsUnsprayed);
+			i.putExtra("sheltersSprayed", sheltersSprayed);
+			i.putExtra("sheltersUnsprayed", sheltersUnsprayed);
+			i.putExtra("canRefilled", refilled);
+			startActivity(i);	
+		}
+	}
 }
