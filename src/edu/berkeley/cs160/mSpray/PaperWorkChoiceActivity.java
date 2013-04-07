1f00cb557791d5a05276a3a3616a591ee67a24ff
Daniel Wu <ldanielcwu@berkeley.edu>
2013-04-06 17:27:29 -0700

    other chemical used work

--- src/edu/berkeley/cs160/mSpray/PaperWorkChoiceActivity.java
+++ src/edu/berkeley/cs160/mSpray/PaperWorkChoiceActivity.java
@@ -7,58 +7,58 @@ import android.view.View;
 import android.widget.Button;
 
 public class PaperWorkChoiceActivity extends Activity {
-	Button pyrethroid;
-	Button ddt;
-	Button noSpray;
-	Button back;
+    Button pyrethroid;
+    Button ddt;
+    Button noSpray;
+    Button back;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.chemical_used);
-		
-		back = (Button)findViewById(R.id.chemical_used_button_back);
-		ddt = (Button)findViewById(R.id.chemical_used_button_ddt);
-		pyrethroid = (Button)findViewById(R.id.chemical_used_button_pyrethroid);
-		noSpray = (Button)findViewById(R.id.chemical_used_button_noSpray);
-		
-		back.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Intent intent = new Intent(getApplicationContext(),
-						GetGpsActivity.class);
-				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				startActivity(intent);
-			};
-		});
-		
-		ddt.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-//				Intent intent = new Intent(getApplicationContext(),
-//						DDTf.class);
-//				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//				startActivity(intent);
-			};
-		});
-		
-		pyrethroid.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Intent intent = new Intent(getApplicationContext(),
-						);
-				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				startActivity(intent);
-			};
-		});
-		
-		noSpray.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Intent intent = new Intent(getApplicationContext(),
-						NoSprayActivity.class);
-				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				startActivity(intent);
-			};
-		});
-		
-		
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chemical_used);
 
-	}
+        back = (Button) findViewById(R.id.chemical_used_button_back);
+        ddt = (Button) findViewById(R.id.chemical_used_button_ddt);
+        pyrethroid = (Button) findViewById(R.id.chemical_used_button_pyrethroid);
+        noSpray = (Button) findViewById(R.id.chemical_used_button_noSpray);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GetGpsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            };
+        });
+
+        ddt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent intent = new Intent(getApplicationContext(),
+                // DDTf.class);
+                // intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                // startActivity(intent);
+            };
+        });
+
+        pyrethroid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent intent = new Intent(getApplicationContext(),
+                // );
+                // intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                // startActivity(intent);
+            };
+        });
+
+        noSpray.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NoSprayActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            };
+        });
+
+    }
 }
--- src/edu/berkeley/cs160/mSpray/SecondChemicalUsedActivity.java
+++ src/edu/berkeley/cs160/mSpray/SecondChemicalUsedActivity.java
@@ -2,6 +2,8 @@ package edu.berkeley.cs160.mSpray;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondChemicalUsedActivity extends Activity {
 
@@ -9,5 +11,16 @@ public class SecondChemicalUsedActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_chemical_used);
+
+        Bundle extras = this.getIntent().getExtras();
+        String firstChemical = extras.getString(Constants.FIRST_CHEMICAL_USED);
+        if (firstChemical.equals(Constants.DDT)) {
+            Button ddtButton = (Button) findViewById(R.id.second_chemical_used_button_ddt);
+            ddtButton.setVisibility(View.GONE);
+        } else {
+            // If the first chemical wasn't DDT, then it was a pyrethroid
+            Button pyrethroidButton = (Button) findViewById(R.id.second_chemical_used_button_pyrethroid);
+            pyrethroidButton.setVisibility(View.INVISIBLE);
+        }
     }
 }
