8ebd4ea57eef25ff055f9ba86dbe7aadde7d26da
Jeffery Butler <butler.jeff1@gmail.com>
2013-05-04 17:09:15 -0700

    Fixed a little bug in textview

--- mSprayApp/src/edu/berkeley/cs160/Base/BaseMainActivity.java
+++ mSprayApp/src/edu/berkeley/cs160/Base/BaseMainActivity.java
@@ -13,8 +13,9 @@ import android.os.Bundle;
  * activity and you scan the rfid. 
  */
 public class BaseMainActivity extends Activity {
-	private NfcAdapter mAdapter; 
+	private NfcAdapter mAdapter;
 	private PendingIntent pendingIntent;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,31 +23,35 @@ public class BaseMainActivity extends Activity {
 		if (mAdapter != null)
 			enableOverrideNexusRfidReader();
 	}
-	
+
 	private void enableOverrideNexusRfidReader() {
 		// set up a PendingIntent to open the app when a tag is scanned
 		mAdapter = NfcAdapter.getDefaultAdapter(this);
-		pendingIntent = PendingIntent.getActivity(
-		  this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+		pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
+				getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 	}
-	
-	/*8
-	 * (non-Javadoc)
-	 * @see android.app.Activity#onResume()
-	 * this adapter only works when the activity is in resume.
+
+	/*
+	 * 8 (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onResume() this adapter only works when the
+	 * activity is in resume.
 	 */
-	public void onResume(){
+	public void onResume() {
 		super.onResume();
-		mAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+		if (mAdapter != null)
+			mAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
 	}
-	
-	public void onPause(){
+
+	public void onPause() {
 		super.onPause();
-		mAdapter.disableForegroundDispatch(this);
+		if (mAdapter != null)
+			mAdapter.disableForegroundDispatch(this);
 	}
-	
+
 	@Override
 	public void onNewIntent(Intent intent) {
-			// do nothing. We will override this in the activities that actually need the RFID scanning abilities
+		// do nothing. We will override this in the activities that actually
+		// need the RFID scanning abilities
 	}
 }
